9565788395829af00666cf894e487353e8d000b7
CooperE314 <127431439+CooperE314@users.noreply.github.com>
2024-04-16 11:15:01 -0700

    Update Maps.cpp
    function to call maps when we run main, and a simple test map

--- Maps.cpp
+++ Maps.cpp
@@ -1,1 +1,13 @@
-//maps
+#include <Vector>
+#include <string>
+
+auto GetMaps(int Num){
+    std::vector<std::string> Map1=
+            {"..#..",
+             "..#..",
+             "..#..",
+             "..#..",
+             "....."};
+
+    if (Num = 1){return Map1;};
+}
