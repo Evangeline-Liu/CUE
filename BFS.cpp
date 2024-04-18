c088245ee417af0430bab9359e1122e0ed3767f6
CooperE314 <127431439+CooperE314@users.noreply.github.com>
2024-04-18 09:42:55 -0700

    Update BFS.cpp
    adds all diagonal movements, and checks the # for 1x1 movements

--- BFS.cpp
+++ BFS.cpp
@@ -14,12 +14,40 @@ bool isValid(int x, int y, int rows, int cols, std::vector<std::string> map, int
     if (map[x][y] == '#') {
         return false;
     }
-    int steps = max(abs(dx) , abs(dy));
-    for (int step = 1; step < steps; ++step) {
-        int newX = x - (dx / steps) * step;
-        int newY = y - (dy / steps) * step;
-        if (map[newX][newY] == '#')
-            return false;
+    //if not diagonal
+    if (dx == 0 || dy == 0) {
+        int steps = max(abs(dx), abs(dy));
+        for (int step = 1; step < steps; ++step) {
+            int newX = x - (dx / steps) * step;
+            int newY = y - (dy / steps) * step;
+            if (map[newX][newY] == '#')
+                return false;
+        }
+    }
+    //diagonal
+    if (abs(dx)>0 && abs(dy)>0){
+        //1x1
+        if (dx ==1 && dy == 1) { // Moving down-right
+            if (map[x][y-1] == '#' || map[x-1][y] == '#') {
+                return false;
+            }
+        }
+        else if (dx ==1 && dy == -1) { // Moving down-left
+            if (map[x][y+1] == '#' || map[x-1][y] == '#') {
+                return false;
+            }
+        }
+        else if (dx == -1 && dy ==1) { // Moving up-right
+            if (map[x][y-1] == '#' || map[x+1][y] == '#') {
+                return false;
+            }
+        }
+        else if (dx == -1 && dy== -1) { // Moving up-left
+            if (map[x][y+1] == '#' || map[x+1][y] == '#') {
+                return false;
+            }
+        }
+        //2x1 moves
     }
     return true;
 }
@@ -35,8 +63,12 @@ int BFS(vector<string> grid, pair<int, int> start, pair<int, int> finish) {
     q.push(start);
     visited[start.first][start.second] = true;
 
-    int dx[] = {-3, -2, -1, 1, 2, 3, 0, 0, 0, 0 ,0 ,0};
-    int dy[] = {0, 0, 0, 0, 0, 0, -3, -2, -1, 1 ,2 ,3};
+    int dx[] = {-3, -2, -1, 1, 2, 3, 0, 0, 0, 0 ,0 ,0,
+                1, 1,2,2, 1, 1, 2, 2,-1,-1,-2,-2,
+                -1,-1,-2,-2};
+    int dy[] = {0, 0, 0, 0, 0, 0, -3, -2, -1, 1 ,2 ,3,
+               1, 2,1,2, -1,-2,-1,-2,1,2,1,2,
+               -1,-2,-1,-2};
 
     while (!q.empty()) {
         pair<int, int> curr = q.front();
@@ -61,7 +93,7 @@ int BFS(vector<string> grid, pair<int, int> start, pair<int, int> finish) {
             }
             return shortestPath.size() - 1;
         }
-        for (int i = 0; i < 12; ++i) {
+        for (int i = 0; i < 28; ++i) {
             int newRow = curr.first + dx[i];
             int newCol = curr.second + dy[i];
 
@@ -74,5 +106,4 @@ int BFS(vector<string> grid, pair<int, int> start, pair<int, int> finish) {
     }
     return -1;
 }
-
 
