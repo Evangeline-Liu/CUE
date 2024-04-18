fb2e9bf0ad3ece8a4d01497d434067145b860fdb
CooperE314 <127431439+CooperE314@users.noreply.github.com>
2024-04-18 09:41:21 -0700

    Update Maps.cpp
    add more map

--- Maps.cpp
+++ Maps.cpp
@@ -1,13 +1,38 @@
-#include <Vector>
-#include <string>
+#include "Maps.h"
 
-auto GetMaps(int Num){
-    std::vector<std::string> Map1=
-            {"..#..",
-             "..#..",
-             "..#..",
-             "..#..",
-             "....."};
+std::vector<std::string> GetMaps(int Num) {
+    std::vector<std::string> Map1 = {
+            "..#..",
+            "..#..",
+            "..#..",
+            "..#..",
+            "....."
+    };
+    std::vector<std::string> Map2 = {
+            "..#..",
+            "..#..",
+            "#....",
+            "#..#.",
+            "...#."
+    };
+    std::vector<std::string> Map3 = {
+            "....#..#..",
+            "...#......",
+            "..#..#..#.",
+            ".#..#....#",
+            "...#..#..."
+    };
 
-    if (Num = 1){return Map1;};
+    if (Num == 1) {
+        return Map1;
+    }
+    else if (Num==2) {
+        return Map2;
+    }
+    else if (Num ==3 ){
+        return Map3;
+    }
+    else {
+        return {};
+    }
 }
