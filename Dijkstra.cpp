b244113a189c3f26eb8a8b98cba742f5f83f3250
Evangeline-Liu <127452252+Evangeline-Liu@users.noreply.github.com>
2024-04-20 12:56:14 -0400

    Update Dijkstra.cpp
    Added stuff for > and < possible extra stuff and need to remove main after and figure out what to do with the return later.

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -8,7 +7,7 @@
 #include <list>
 using namespace std;
 
-//
+// things to remember
 typedef pair<int,int> coords;
 pair<coords,float>* coords_distance;
 
@@ -16,6 +15,7 @@ pair<coords,float>* coords_distance;
 class graph{
 
 public:
+    
     // to coord, to from coord, distance
     map<coords,pair<coords,float>> least_dist;
 
@@ -25,6 +25,7 @@ public:
     // string of the actual map
     std::vector<std::string> map_board;
 
+    // starting position and ending position
     coords start;
     coords end;
 
@@ -33,6 +34,7 @@ public:
         for(int i = 0; i < map_board.size(); i++){
             for(int j = 0; j < map_board[0].size(); j++){
 
+                //  when reading strings it is backwards y,x way
                 if(map_board[i][j] == 'e'){
                     end = make_pair(j,i);
                 }
@@ -43,6 +45,67 @@ public:
             }
         }
     }
+
+    pair<pair<int,int>,bool> count_hilly(coords to, coords from){
+
+        pair<pair<int,int>,bool> returnie = make_pair(make_pair(0,0),true);
+        int x_to = to.first;
+        int y_to = to.second;
+
+        int x_from = from.first;
+        int y_from = from.second;
+
+        int x_temp = 0;
+        int x_temp_end = 0;
+
+        if( x_to > x_from){
+            x_temp = x_from;
+            x_temp_end = x_to;
+        }else{
+            x_temp = x_to;
+            x_temp_end = x_from;
+        }
+
+        int y_temp = 0;
+        int y_temp_end = 0;
+        if( y_to > y_from){
+            y_temp = y_from;
+            y_temp_end = y_to;
+        }else{
+            y_temp = y_to;
+            y_temp_end = y_from;
+        }
+
+        if(x_temp_end == x_temp){
+            for(int i = y_temp; i <= y_temp_end; i++){
+                if(map_board[i][x_temp] == '#' ){
+                    returnie.second = false;
+                }
+
+                if(map_board[i][x_temp] == '>' ){
+                    returnie.first.first += 1;
+                }
+                if(map_board[i][x_temp] == '<' ){
+                    returnie.first.first -= 1;
+                }
+            }
+        }else{
+            for(int i = x_temp; i <= x_temp_end; i++){
+                if(map_board[i][x_temp] == '#' ){
+                    returnie.second = false;
+                }
+                if(map_board[y_temp][i] == '>'){
+                    returnie.first.first += 1;
+                }
+                if(map_board[y_temp][i] == '<'){
+                    returnie.first.first -= 1;
+                }
+            }
+        }
+
+        return returnie;
+    }
+
     bool check_straights(coords to, coords from){
         int x_to = to.first;
         int y_to = to.second;
@@ -89,6 +152,7 @@ public:
 
         return true;
     }
+
     bool check_diagonals(coords to, coords from){
         int x_to = to.first;
         int y_to = to.second;
@@ -175,6 +239,38 @@ public:
 
                 // check if open
                 if(!(map_board[ny][nx] == 'e' || map_board[ny][nx] == 's' || map_board[ny][nx] == '.')){
+                    // check if >> or  << uphill or downhill
+                    // run separate code
+
+                    if(map_board[ny][nx] == '>'){
+                        float dist_calc = sqrt(pow(dx[i],2)+pow(dy[i],2))+distance;
+                        if (least_dist.find(to) == least_dist.end()){
+                            least_dist[to] = make_pair(from, dist_calc);
+                            to.first = to.first+1;
+                            temp.push(make_pair(to, dist_calc));
+                        }else {
+                            if (least_dist[to].second > dist_calc) {
+                                least_dist[to] = make_pair(from, dist_calc);
+                                to.first = to.first+1;
+                                temp.push(make_pair(to, dist_calc));
+                            }
+                        }
+                    }
+                    if(map_board[ny][nx] == '<'){
+                        float dist_calc = sqrt(pow(dx[i],2)+pow(dy[i],2))+distance;
+                        if (least_dist.find(to) == least_dist.end()){
+                            least_dist[to] = make_pair(from, dist_calc);
+                            to.first = to.first-1;
+                            temp.push(make_pair(to, dist_calc));
+                        }else {
+                            if (least_dist[to].second > dist_calc) {
+                                least_dist[to] = make_pair(from, dist_calc);
+                                to.first = to.first-1;
+                                temp.push(make_pair(to, dist_calc));
+                            }
+                        }
+                    }
+                    // just skipie always
                     continue;
                 }
                 // check if possible diagonal
@@ -186,7 +282,28 @@ public:
 
 
                 // check normal spaces
+                // should change and fix so they can find if it exists
                 if(!check_straights(to,from)){
+                    // there are blocks and you should see if its >> or <<
+                    pair<pair<int,int>,bool> holding = count_hilly(to,from);
+                    if(holding.second){
+                        float dist_calc = sqrt(pow(dx[i],2)+pow(dy[i],2))+distance;
+
+                        // what if least_dist doesn't exist?
+                        if (least_dist.find(to) == least_dist.end()){
+                            least_dist[to] = make_pair(from, dist_calc);
+                            to.first += holding.first.first;
+                            to.second += holding.first.second;
+                            temp.push(make_pair(to, dist_calc));
+                        }else {
+                            if (least_dist[to].second > dist_calc) {
+                                least_dist[to] = make_pair(from, dist_calc);
+                                to.first += holding.first.first;
+                                to.second += holding.first.second;
+                                temp.push(make_pair(to, dist_calc));
+                            }
+                        }
+                    }
                     continue;
                 }
 
@@ -211,12 +328,16 @@ public:
     }
 
     list<coords> Shortest_Path(){
+
         list<coords> returne;
         returne.push_back(end);
         float temper = least_dist[end].second;
+        cout << "Distance Traveled: "<<temper << endl;
+
         coords temp_coord = least_dist[end].first;
         returne.push_back(temp_coord);
         temper = least_dist[temp_coord].second;
+
         while(temper != 0){
 
             temp_coord = least_dist[temp_coord].first;
@@ -258,11 +379,13 @@ int main() {
             ".#..#....#",
             "...#..#..e"
     };
+
     graph temp = graph(Map3);
     temp.Mapping_Out();
 
     list<coords> answer = temp.Shortest_Path();
-
+    cout << "Number of Shots: " << answer.size()-1 << endl;
+    cout << "Shots Taken:" << endl;
     for (auto it = answer.begin(); it != answer.end(); ++it) {
         cout << it->first << ", " << it->second << endl;
     }
