d8d8374f027fde1ba54184d27cda44ffc7cab8ba
CooperE314 <127431439+CooperE314@users.noreply.github.com>
2024-04-22 10:25:21 -0700

    Update BFS.cpp
    complete algorithm

--- BFS.cpp
+++ BFS.cpp
@@ -1,12 +1,15 @@
+
+
 #include <iostream>
 #include <vector>
 #include <string>
 #include <queue>
+#include <valarray>
 #include "algorithm"
 using namespace std;
 
 // function to see if valid position
-bool isValid(int x, int y, int rows, int cols, std::vector<std::string> map, int dx, int dy)
+bool isValid(int x, int y, int rows, int cols, std::vector<std::string> map, int dx, int dy, int& hills)
 {
     if (x < 0 || x >= rows || y < 0 || y >= cols) {
         return false;
@@ -20,9 +23,19 @@ bool isValid(int x, int y, int rows, int cols, std::vector<std::string> map, int
         for (int step = 1; step < steps; ++step) {
             int newX = x - (dx / steps) * step;
             int newY = y - (dy / steps) * step;
-            if (map[newX][newY] == '#')
+            if (map[newX][newY] == '#') {
                 return false;
+            }
+            if (map[newX][newY] == '<'){
+                hills -=1;
+
+            }
+            if (map[newX][newY] == '>'){
+                hills +=1;
+            }
+
         }
+
     }
     //diagonal
     if (abs(dx)>0 && abs(dy)>0){
@@ -31,14 +44,32 @@ bool isValid(int x, int y, int rows, int cols, std::vector<std::string> map, int
             if (map[x][y-1] == '#' || map[x-1][y] == '#') {
                 return false;
             }
+            if (map[x][y-1] == '<' || map[x-1][y] == '<') {
+                return false;
+            }
+            if (map[x][y-1] == '>' || map[x-1][y] == '>') {
+                return false;
+            }
         }
         else if (dx ==1 && dy == -1) { // Moving down-left
             if (map[x][y+1] == '#' || map[x-1][y] == '#') {
                 return false;
             }
+            if (map[x][y+1] == '<' || map[x-1][y] == '<') {
+                return false;
+            }if (map[x][y+1] == '>' || map[x-1][y] == '>') {
+                return false;
+            }
+
         }
         else if (dx == -1 && dy ==1) { // Moving up-right
             if (map[x][y-1] == '#' || map[x+1][y] == '#') {
+                return false;
+            }
+            if (map[x][y-1] == '<' || map[x+1][y] == '<') {
+                return false;
+            }
+            if (map[x][y-1] == '>' || map[x+1][y] == '>') {
                 return false;
             }
         }
@@ -46,24 +77,58 @@ bool isValid(int x, int y, int rows, int cols, std::vector<std::string> map, int
             if (map[x][y+1] == '#' || map[x+1][y] == '#') {
                 return false;
             }
+            if (map[x][y+1] == '<' || map[x+1][y] == '<') {
+                return false;
+            }
+            if (map[x][y+1] == '>' || map[x+1][y] == '>') {
+                return false;
+            }
+
         }
         //2x1 moves
         else if (dx == 2 && dy == 1){
             if (map[x-1][y] == '#' || map[x][y-1] == '#' || map[x-1][y-1] == '#' ){
                 return false;
             }
+            if (map[x-1][y] == '<' || map[x][y-1] == '<' || map[x-1][y-1] == '<' ){
+                return false;
+            }
+            if (map[x-1][y] == '>' || map[x][y-1] == '>' || map[x-1][y-1] == '>' ){
+                return false;
+            }
+
         }
         else if (dx == 2 && dy == -1) { // Moving down-left
             if (map[x - 1][y] == '#' || map[x][y + 1] == '#' || map[x - 1][y + 1] == '#') {
-                return false; // Obstacle in down-left direction
+                return false;
             }
-        } else if (dx == -2 && dy == 1) { // Moving up-right
+            if (map[x - 1][y] == '<' || map[x][y + 1] == '<' || map[x - 1][y + 1] == '<') {
+                return false;
+            }
+            if (map[x - 1][y] == '>' || map[x][y + 1] == '>' || map[x - 1][y + 1] == '>') {
+                return false;
+            }
+        }
+        else if (dx == -2 && dy == 1) { // Moving up-right
             if (map[x + 1][y] == '#' || map[x][y - 1] == '#' || map[x + 1][y - 1] == '#') {
-                return false; // Obstacle in up-right direction
+                return false;
             }
-        } else if (dx == -2 && dy == -1) { // Moving up-left
+            if (map[x + 1][y] == '<' || map[x][y - 1] == '<' || map[x + 1][y - 1] == '<') {
+                return false;
+            }
+            if (map[x + 1][y] == '>' || map[x][y - 1] == '>' || map[x + 1][y - 1] == '>') {
+                return false;
+            }
+        }
+        else if (dx == -2 && dy == -1) { // Moving up-left
             if (map[x + 1][y] == '#' || map[x][y + 1] == '#' || map[x + 1][y + 1] == '#') {
-                return false; // Obstacle in up-left direction
+                return false;
+            }
+            if (map[x + 1][y] == '<' || map[x][y + 1] == '<' || map[x + 1][y + 1] == '<') {
+                return false;
+            }
+            if (map[x + 1][y] == '>' || map[x][y + 1] == '>' || map[x + 1][y + 1] == '>') {
+                return false;
             }
         }
         //1x2moves
@@ -71,40 +136,89 @@ bool isValid(int x, int y, int rows, int cols, std::vector<std::string> map, int
             if (map[x][y-1] == '#' || map[x-1][y] == '#' || map[x-1][y-1] == '#'){
                 return false;
             }
+            if (map[x][y-1] == '<' || map[x-1][y] == '<' || map[x-1][y-1] == '<'){
+                return false;
+            }
+            if (map[x][y-1] == '>' || map[x-1][y] == '>' || map[x-1][y-1] == '>'){
+                return false;
+            }
         }
         else if (dx == 1 && dy == -2) { // Moving down-left
             if (map[x][y - 1] == '#' || map[x - 1][y] == '#' || map[x - 1][y + 1] == '#') {
-                return false; // Obstacle in down-left direction
+                return false;
+            }
+            if (map[x][y - 1] == '<' || map[x - 1][y] == '<' || map[x - 1][y + 1] == '<') {
+                return false;
+            }
+            if (map[x][y - 1] == '>' || map[x - 1][y] == '>' || map[x - 1][y + 1] == '>') {
+                return false;
             }
         }
         else if (dx == -1 && dy == 2) { // Moving up-right
             if (map[x][y + 1] == '#' || map[x + 1][y] == '#' || map[x + 1][y - 1] == '#') {
-                return false; // Obstacle in up-right direction
+                return false;
+            }
+            if (map[x][y + 1] == '<' || map[x + 1][y] == '<' || map[x + 1][y - 1] == '<') {
+                return false;
+            }
+            if (map[x][y + 1] == '>' || map[x + 1][y] == '>' || map[x + 1][y - 1] == '>') {
+                return false;
             }
         }
         else if (dx == -1 && dy == -2) { // Moving up-left
             if (map[x][y + 1] == '#' || map[x + 1][y] == '#' || map[x + 1][y + 1] == '#') {
-                return false; // Obstacle in up-left direction
+                return false;
+            }
+            if (map[x][y + 1] == '<' || map[x + 1][y] == '<' || map[x + 1][y + 1] == '<') {
+                return false;
+            }
+            if (map[x][y + 1] == '>' || map[x + 1][y] == '>' || map[x + 1][y + 1] == '>') {
+                return false;
             }
         }
+        //2x2
         else if (dx == 2 && dy == 2){
             if (map[x][y-1] == '#' || map[x-1][y] == '#' ||map[x-1][y-1] == '#' || map[x-2][y-1]=='#' || map[x-1][y-2]=='#'){
+                return false;
+            }
+            if (map[x][y-1] == '<' || map[x-1][y] == '<' ||map[x-1][y-1] == '<' || map[x-2][y-1]=='<' || map[x-1][y-2]=='<'){
+                return false;
+            }
+            if (map[x][y-1] == '>' || map[x-1][y] == '>' ||map[x-1][y-1] == '>' || map[x-2][y-1]=='>' || map[x-1][y-2]=='>'){
                 return false;
             }
         }
         else if (dx == 2 && dy == -2) { // Moving down-left
             if (map[x][y - 1] == '#' || map[x - 1][y] == '#' || map[x - 1][y + 1] == '#' || map[x - 2][y + 1] == '#' || map[x - 1][y + 2] == '#') {
-                return false; // Obstacle in down-left direction
+                return false;
+            }
+            if (map[x][y - 1] == '<' || map[x - 1][y] == '<' || map[x - 1][y + 1] == '<' || map[x - 2][y + 1] == '<' || map[x - 1][y + 2] == '<') {
+                return false;
+            }
+            if (map[x][y - 1] == '>' || map[x - 1][y] == '>' || map[x - 1][y + 1] == '>' || map[x - 2][y + 1] == '>' || map[x - 1][y + 2] == '>') {
+                return false;
             }
         }
         else if (dx == -2 && dy == 2) { // Moving up-right
             if (map[x][y + 1] == '#' || map[x + 1][y] == '#' || map[x + 1][y - 1] == '#' || map[x + 2][y - 1] == '#' || map[x + 1][y - 2] == '#') {
-                return false; // Obstacle in up-right direction
+                return false;
+            }
+            if (map[x][y + 1] == '<' || map[x + 1][y] == '<' || map[x + 1][y - 1] == '<' || map[x + 2][y - 1] == '<' || map[x + 1][y - 2] == '<') {
+                return false;
+            }
+            if (map[x][y + 1] == '>' || map[x + 1][y] == '>' || map[x + 1][y - 1] == '>' || map[x + 2][y - 1] == '>' || map[x + 1][y - 2] == '>') {
+                return false;
             }
         }
         else if (dx == -2 && dy == -2) { // Moving up-left
             if (map[x][y + 1] == '#' || map[x + 1][y] == '#' || map[x + 1][y + 1] == '#' || map[x + 2][y + 1] == '#' || map[x + 1][y + 2] == '#') {
-                return false; // Obstacle in up-left direction
+                return false;
+            }
+            if (map[x][y + 1] == '<' || map[x + 1][y] == '<' || map[x + 1][y + 1] == '<' || map[x + 2][y + 1] == '<' || map[x + 1][y + 2] == '<') {
+                return false;
+            }
+            if (map[x][y + 1] == '>' || map[x + 1][y] == '>' || map[x + 1][y + 1] == '>' || map[x + 2][y + 1] == '>' || map[x + 1][y + 2] == '>') {
+                return false;
             }
         }
     }
@@ -117,11 +231,10 @@ int BFS(vector<string> grid, pair<int, int> start, pair<int, int> finish) {
 
     vector<vector<bool>> visited(numRows, vector<bool>(numCols, false));
     vector<vector<pair<int, int>>> parent(numRows, vector<pair<int, int>>(numCols, {-1, -1}));
-
+    vector<vector<int>> distance(numRows, vector<int>(numCols, 0));
     queue<pair<int, int>> q;
     q.push(start);
     visited[start.first][start.second] = true;
-
     int dx[] = {-3, -2, -1, 1, 2, 3, 0, 0, 0, 0 ,0 ,0,
                 1, 1,2,2, 1, 1, 2, 2,-1,-1,-2,-2,
                 -1,-1,-2,-2};
@@ -142,7 +255,7 @@ int BFS(vector<string> grid, pair<int, int> start, pair<int, int> finish) {
             shortestPath.push_back(start);
             for (int i = 0; i < numRows; ++i) {
                 for (int j = 0; j < numCols; ++j) {
-                    if (find(shortestPath.begin(), shortestPath.end(), make_pair(i, j)) != shortestPath.end()) {
+                    if (find(shortestPath.begin(), shortestPath.end(), make_pair(i, j)) != shortestPath.end() &&grid[i][j]!='<') {
                         cout << "o ";
                     } else {
                         cout << grid[i][j] << " ";
@@ -150,16 +263,30 @@ int BFS(vector<string> grid, pair<int, int> start, pair<int, int> finish) {
                 }
                 cout << endl;
             }
+            std::cout <<"Distance " << distance[finish.first][finish.second] + 1<< std::endl;
             return shortestPath.size() - 1;
         }
         for (int i = 0; i < 28; ++i) {
             int newRow = curr.first + dx[i];
             int newCol = curr.second + dy[i];
+            int hills = 0;
+            if (isValid(newRow, newCol, numRows, numCols, grid, dx[i] ,dy[i], hills) && !visited[newRow][newCol]) {
+                newCol+=hills;
+                if (grid[newRow][newCol]== '>'){
+                    //newCol+=1;
+                    q.push({newRow, newCol+1});
+                    visited[newRow][newCol+1] = true;
+                    parent[newRow][newCol+1] = curr;
+                }
+                int dist = 0;
+                dist +=sqrt(pow(dx[i], 2) + pow(dy[i],2));
 
-            if (isValid(newRow, newCol, numRows, numCols, grid, dx[i] ,dy[i]) && !visited[newRow][newCol]) {
+                dist += distance[curr.first][curr.second];
+
                 q.push({newRow, newCol});
                 visited[newRow][newCol] = true;
                 parent[newRow][newCol] = curr;
+                distance[newRow][newCol] = dist;
             }
         }
     }
