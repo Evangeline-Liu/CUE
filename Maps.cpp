f8a8d78b1a514305a351b7d8fb05d4bf3cda13ce
CooperE314 <127431439+CooperE314@users.noreply.github.com>
2024-04-22 10:41:35 -0700

    Update Maps.cpp
    7 maps

--- Maps.cpp
+++ Maps.cpp
@@ -1,12 +1,10 @@
-#include "Maps.h"
-
 std::vector<std::string> GetMaps(int Num) {
     std::vector<std::string> Map1 = {
-            "..#..",
+            "s.#..",
             "..#..",
             ".....",
             "..#..",
-            "..#.."
+            "..#.e"
     };
     std::vector<std::string> Map2 = {
             "...#.",
@@ -32,6 +30,20 @@ std::vector<std::string> GetMaps(int Num) {
     std::vector<std::string> Map5 = {
             ".>>>>."
     };
+    std::vector<std::string> Map6 = {
+            ".#....##..",
+            ".#.#..#...",
+            ".#.#..#.#.",
+            ".#.#..#.#.",
+            "...#....#."
+    };
+    std::vector<std::string> Map7 = {
+            "...#......",
+            "...#......",
+            "...#......",
+            "...#......",
+            "...#......"
+    };
 
     if (Num == 1) {
         return Map1;
@@ -47,6 +59,12 @@ std::vector<std::string> GetMaps(int Num) {
     }
     else if (Num ==5 ){
         return Map5;
+    }
+    else if (Num ==6 ){
+        return Map6;
+    }
+    else if (Num ==7 ){
+        return Map7;
     }
     else {
         return {};
